07d8afa2a1897c671314fc3310bcf200c3ea3fa9
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-11-03 04:24:46 +0530

    initial

---
+++ main.js
@@ -0,0 +1,42 @@
+let cellsRow = 100;
+let cellsColumn = 26;
+
+const addressColCont = document.querySelector(".address-col-cont");
+const addressRowCont = document.querySelector(".address-row-cont");
+const cellsCont = document.querySelector(".cells-cont");
+const addressBar = document.querySelector(".address-bar");
+
+for (i = 0; i < cellsRow; i++) {
+  let addressCol = document.createElement("div");
+  addressCol.setAttribute("class", "address-col");
+  addressCol.innerHTML = i + 1;
+  addressColCont.appendChild(addressCol);
+}
+
+for (i = 0; i < cellsColumn; i++) {
+  let addressRow = document.createElement("div");
+  addressRow.setAttribute("class", "address-row");
+  addressRow.innerHTML = String.fromCharCode(65 + i);
+  addressRowCont.appendChild(addressRow);
+}
+
+for (i = 0; i < cellsRow; i++) {
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class", "row-cont");
+  for (j = 0; j < cellsColumn; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contenteditable", "true");
+    addListnerCellAddress(cell, i, j);
+    rowCont.appendChild(cell);
+  }
+  cellsCont.appendChild(rowCont);
+}
+
+function addListnerCellAddress(cell, i, j) {
+  cell.addEventListener("click", () => {
+    let rowId = i + 1;
+    let colId = String.fromCharCode(65 + j);
+    addressBar.value = `${colId}${rowId}`;
+  });
+}
